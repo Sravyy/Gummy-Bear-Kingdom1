32064976b7f42d68ab57add5bda9dc759406b1fa
sravanthi velaga <svelaga0412@gmail.com>
2018-01-05 16:03:23 -0800

    Add Home About and Contact Views

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GummyBearKingdom.Models;
+
+
+namespace GummyBearKingdom.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private GummyBearKingdomDbContext db = new GummyBearKingdomDbContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {   
+            return View(db.Products.ToList());
+        }
+
+
+
+    }
+}
